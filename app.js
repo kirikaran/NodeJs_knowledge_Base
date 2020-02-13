d6802f28c63639a1390d141cc18ac332f8445f76
Kirikaran <kirikarankiri63@gmail.com>
2020-02-13 13:04:28 +0530

    views setup and basic for frontend

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
 const express =require('express');
+const path=require('path');
 
+//init app
 const app=express();
 
+//load engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+//home route
 app.get('/',function(req,res){
-    res.send('hello world');
+    let articles=[
+        {
+            id:1,
+            title:'Articles One',
+            author:'Brand SGIC',
+            body:'this is article one'
+        },
+        {
+            id:2,
+            title:'Articles Two',
+            author:'Brand SGIC',
+            body:'this is article two'
+        },
+        {
+            id:3,
+            title:'Articles Three',
+            author:'Brand SGIC',
+            body:'this is article three'
+        }
+    ]
+    res.render('index',{
+
+        tittle:'Articles',
+        articles:articles
+    });
+    
+
 });
 
+//Add Route
+app.get('/articles/add',function(req,res){
+    res.render('add_article',{
+        tittle:'Add Article'
+    })
+})
+
+//start server
 app.listen(3000,function(){
     console.log('server started on port 3000...');
 }
