44f171f365377079003dabefb8672a296ee068f2
Kirikaran <kirikarankiri63@gmail.com>
2020-02-13 15:45:40 +0530

    mongoose model setup

--- app.js
+++ app.js
@@ -1,42 +1,48 @@
 const express =require('express');
 const path=require('path');
+const mongoose=require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/nodekb');
+let db=mongoose.connection;
+
+//check connection
+db.once('open',function(){
+    console.log('connected to MongoDB');
+});
+
+//check for DB errors
+db.on('error',function(err){
+    console.log(err);
+});
 
 //init app
 const app=express();
 
+//Bring in Model
+let Article=require('./models/article');
 //load engine
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
 //home route
+
+
 app.get('/',function(req,res){
-    let articles=[
-        {
-            id:1,
-            title:'Articles One',
-            author:'Brand SGIC',
-            body:'this is article one'
-        },
-        {
-            id:2,
-            title:'Articles Two',
-            author:'Brand SGIC',
-            body:'this is article two'
-        },
-        {
-            id:3,
-            title:'Articles Three',
-            author:'Brand SGIC',
-            body:'this is article three'
-        }
-    ]
-    res.render('index',{
+   Article.find({},function(err,articles){
+       if(err){
+           console.log(err);
+       }else{
+   
+           //console.log(articles)
+           res.render('index',{
 
-        tittle:'Articles',
-        articles:articles
-    });
+            title:'Articles',
+            articles:articles
+        });
     
-
+}
+   });
 });
+    
 
 //Add Route
 app.get('/articles/add',function(req,res){
@@ -48,5 +54,4 @@ app.get('/articles/add',function(req,res){
 //start server
 app.listen(3000,function(){
     console.log('server started on port 3000...');
-}
-)
+});
