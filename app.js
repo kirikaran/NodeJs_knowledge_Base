6be422cc154900ce320986655a1c7f36d3b2221f
Kirikaran <kirikarankiri63@gmail.com>
2020-02-14 11:12:58 +0530

    Update and delete

--- app.js
+++ app.js
@@ -86,6 +86,49 @@ app.post('/articles/add',function(req,res){
     });
 });
 
+//Load Edit Form
+app.get('/article/edit/:id', function(req,res){
+    Article.findById(req.params.id,function(err,article){
+        res.render('edit_article',{
+            title:'Edit Article',
+            article:article
+        });
+    });
+});
+
+//Update submit post route
+app.post('/articles/edit/:id',function(req,res){
+    let article={};
+    article.title=req.body.title;
+    article.author=req.body.author;
+    article.body=req.body.body;
+
+    let query={_id:req.params.id}
+
+    Article.update(query,article,function(err){
+        if(err){
+            console.log(err);
+            return;
+        }else {
+            res.redirect('/')
+        }
+    });
+});
+
+//Delete Article
+app.delete('/article/:id',function(req,res){
+    let query= {_id:req.params.id}
+
+    Article.remove(query,function(err){
+        if(err){
+            console.log(err);
+        }
+        res.send('Success');
+    })
+})
+
+    
+
 //start server
 app.listen(3000,function(){
     console.log('server started on port 3000...');
